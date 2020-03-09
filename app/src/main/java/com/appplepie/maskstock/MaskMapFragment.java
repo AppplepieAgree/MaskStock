ac18f141f4e8c67b837671042635ec46da0c3115
yechan0704 <key07040@naver.com>
2020-03-10 00:32:09 +0900

    bottom navigation view

---
+++ app/src/main/java/com/appplepie/maskstock/MaskMapFragment.java
@@ -0,0 +1,30 @@
+package com.appplepie.maskstock;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MaskMapFragment extends Fragment {
+
+    public MaskMapFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View view = inflater.inflate(R.layout.fragment_mask_map, container, false);
+        return view;
+    }
+}
--- app/src/main/java/com/appplepie/maskstock/StoreResult.java
+++ app/src/main/java/com/appplepie/maskstock/StoreResult.java
@@ -1,6 +1,14 @@
 package com.appplepie.maskstock;
 
+<<<<<<< HEAD
 class StoreResult {
     int count;
     Store[] stores;
+=======
+public class StoreResult {
+
+    int count;
+    Store[] stores;
+
+>>>>>>> bottom navigation view
 }
