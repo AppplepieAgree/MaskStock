6dc8aca61f4af11659b3e0fa83da5d0692b15ecb
moseori <haojile96@gmail.com>
2020-03-10 00:07:06 +0900

    Store class updated

--- app/src/main/java/com/appplepie/maskstock/Store.java
+++ app/src/main/java/com/appplepie/maskstock/Store.java
@@ -2,14 +2,13 @@ package com.appplepie.maskstock;
 
 class Store {
 
-    String code;
-    String name;
+    String code; // 식별코드
+    String name; // 이름
+    String addr; // 주소
+    String type; // "판매처 유형[약국: '01', 우체국: '02', 농협: '03']"
     double lat;
     double lng;
-    String stock_t;
-    int stock_cnt;
-    int sold_cnt;
-    int remain_cnt;
-    boolean sold_out;
+    int stock_at;
+    int remain_stat; //"재고 상태[100개 이상(녹색): 'plenty' / 30개 이상 100개미만(노랑색): 'some' / 2개 이상 30개 미만(빨강색): 'few' / 1개 이하(회색): 'empty']"
     String created_at;
 }
